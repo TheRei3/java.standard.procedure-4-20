1da6b7a5380a0884b2fd374eaeb4600efe509fae
admin <admin@DESKTOP-8L2IG92>
2021-05-29 06:24:37 +0900

    ex4no20

---
+++ ex4no20/src/ex4no20/ex4no20.java
@@ -0,0 +1,13 @@
+package ex4no20;
+
+public class ex4no20 {  // 165p
+	public static void main(String[] args) {
+		for( int i = 1 ; i <= 5 ; i++) {
+			for( int j = 1 ; j <= 5 ; j++) {
+				System.out.printf("[%d,%d]", i, j);
+			}
+			System.out.println();
+		}
+	}
+
+}
